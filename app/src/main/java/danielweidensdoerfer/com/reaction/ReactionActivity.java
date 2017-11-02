69ad1574607033832c85f67db769ca702c016513
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2017-11-02 19:30:56 +0100

    Initial commit

---
+++ app/src/main/java/danielweidensdoerfer/com/reaction/ReactionActivity.java
@@ -0,0 +1,14 @@
+package danielweidensdoerfer.com.reaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reaction);
+    }
+
+}
